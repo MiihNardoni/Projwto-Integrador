20fcad2ce0f90bf75d2069369cbbcfd4ad131894
Mich Mizita <michellesilva2407@gmail.com>
2023-05-10 09:01:12 -0300

    criação do modelo

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Usuário(models.Model):
+    nome = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=100, unique=True)
+    avatar = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.nickname
+
+class Questionário(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=500, null=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    questionario_col = models.CharField(max_length=45, null=True)
+    capa = models.CharField(max_length=255, null=True)
+    usuário = models.ForeignKey(Usuário, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+class Hashtag(models.Model):
+    tag = models.CharField(max_length=45, unique=True)
+
+    def __str__(self):
+        return self.tag
+
+class Hashtag_has_questionário(models.Model):
+    hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
+    questionário = models.ForeignKey(Questionário, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('hashtag', 'questionário')

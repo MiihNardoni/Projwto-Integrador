02b391d198989b2ab8e6d483cea804363f1c7856
Mich Mizita <michellesilva2407@gmail.com>
2023-06-14 08:51:21 -0300

    changes

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")
+
+def detail(request, question_id):
+    return HttpResponse(f"Você está na enquete {question_id}")
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)

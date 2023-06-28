642f62b25997bf905f76521667456b658a7d339c
Mich Mizita <michellesilva2407@gmail.com>
2023-06-28 08:33:25 -0300

    index

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse(f"Você está na enquete {question_id}")
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
+    response = "Você está olhando os resultados da enquete %s."
     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("Você está votando em uma enquete %s." % question_id)
